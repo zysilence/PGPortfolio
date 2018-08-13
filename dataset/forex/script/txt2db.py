e85d976524274dff67117482620651b820c81b69
zysilence <zysilence329b@gmail.com>
2018-08-13 19:39:07 +0800

    add script that generates db

---
+++ dataset/forex/script/txt2db.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import os
+import sqlite3
+import time
+
+
+def parse_time(d, t):
+    return time.mktime(datetime.strptime(d + t, "%Y%m%d%H%M%S").timetuple())
+
+
+def main():
+    parent_path = os.path.abspath(os.path.pardir)
+    conn = sqlite3.connect(os.path.join(parent_path, 'db', 'forex.db'))
+    cur = conn.cursor()
+    sql = 'CREATE TABLE IF NOT EXISTS History (date INTEGER, coin varchar(20), high FLOAT, low FLOAT, open FLOAT, ' \
+          'close FLOAT, volume FLOAT, quoteVolume FLOAT, weightedAverage FLOAT,PRIMARY KEY (date, coin));'
+    cur.execute(sql)
+
+    for f_name in os.listdir(os.path.join(parent_path, 'raw_data')):
+        if not f_name.endswith('txt'):
+            continue
+
+        f_path = os.path.join(parent_path, 'raw_data', f_name)
+        f = open(f_path)
+        for line in f.readlines():
+            if line.startswith('<'):
+                continue
+
+            items = line.split(',')
+            coin = items[0]
+            d = items[1]
+            t = items[2]
+            open_price = items[3]
+            high = items[4]
+            low = items[5]
+            close = items[6]
+            vol = items[7]
+            d = parse_time(d, t)
+
+            cur.execute('insert into History values(?,?,?,?,?,?,?,?,?)',
+                        (d, coin, high, low, open_price, close, vol, 'NULL', 'NULL'))
+
+    cur.close()
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
