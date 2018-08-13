0c27f3761ffc8033b31b30a258b52cc5dbdc7071
zysilence <zysilence329b@gmail.com>
2018-08-13 10:07:03 +0800

    use forex.db

--- pgportfolio/constants.py
+++ pgportfolio/constants.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 from os import path
 
+"""
 DATABASE_DIR = path.realpath(__file__).\
     replace('pgportfolio/constants.pyc','/database/Data.db').\
     replace("pgportfolio\\constants.pyc","database\\Data.db").\
     replace('pgportfolio/constants.py','/database/Data.db').\
     replace("pgportfolio\\constants.py","database\\Data.db")
+"""
+DATABASE_DIR = path.realpath(__file__). \
+    replace('pgportfolio/constants.pyc','/database/forex.db'). \
+    replace("pgportfolio\\constants.pyc","database\\forex.db"). \
+    replace('pgportfolio/constants.py','/database/forex.db'). \
+    replace("pgportfolio\\constants.py","database\\forex.db")
 CONFIG_FILE_DIR = 'net_config.json'
 LAMBDA = 1e-4  # lambda in loss function 5 in training
    # About time
